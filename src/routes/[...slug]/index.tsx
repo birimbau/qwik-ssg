0e5ded0f48a98d47d753e28a6fd14913023598bf
birimbau <tiagompbneto@gmail.com>
2025-01-02 21:22:38 +0000

    chore: test

--- src/routes/[...slug]/index.tsx
+++ src/routes/[...slug]/index.tsx
@@ -1,5 +1,21 @@
 import { component$ } from "@builder.io/qwik";
+import { useLocation, type StaticGenerateHandler } from "@builder.io/qwik-city";
 
 export default component$(() => {
-  return <div>Hello World - slug</div>;
+  const location = useLocation();
+  return <div>Hello World - slug - {location.params.slug}</div>;
 });
+
+export const onStaticGenerate: StaticGenerateHandler = async () => {
+  console.log("onStaticGenerate");
+  return {
+    params: [
+      {
+        slug: "hello",
+      },
+      {
+        slug: "world",
+      },
+    ],
+  };
+};
